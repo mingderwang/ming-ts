61824dcb363d4bfee3e991169166ccee29c425fa
Ming-der Wang <mingderwang@gmail.com>
2022-05-17 11:18:45 +0800

    do not use index.js in html, use index.ts directly

--- index.ts
+++ index.ts
@@ -1,20 +1,19 @@
 interface Person {
-    age: number,
-    name: string,
-    say(): string
+  age: number
+  name: string
+  say(): string
 }
 
 const mike = {
-    age: 25, 
-    name:"Mike", 
-    say: function() { 
-        return "My name is " + this.name + 
-               " and I'm " + this.age + " years old!"
-    }
+  age: 60,
+  name: 'Ming',
+  say: function () {
+    return 'My name is ' + this.name + " and I'm " + this.age + ' years old!'
+  },
 }
 
 function sayIt(person: Person) {
-    return person.say();
+  return person.say()
 }
 
 console.log(sayIt(mike))
